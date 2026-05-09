dc9ff6561e47a992c5ca5f8a3b9cb4f6d9a10c9e
Anais Fontalvo <anaisfontalvocarrillo@gmail.com>
2026-05-09 01:48:04 -0500

    Frontend
    Creación de carpetas, agregación de fuentes, iconos, y tipografías utilizadas

---
+++ resources/views/componentes/navbar.blade.php
@@ -0,0 +1,21 @@
+{{-- resources/views/componentes/navbar.blade.php --}}
+{{--Este es el código para la barra de navegación (navbar) del sistema SIGER.
+ * La barra de navegación incluye el nombre del sistema, un campo de búsqueda y enlaces para el perfil y cerrar sesión.--}}
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div>
+    <a class="navbar-brand" href="#">SIGER</a>
+  </div>
+
+  <div class = "center-nav">
+     <div class="buscador-container">
+        <i class="fas fa-search search-icon"></i>
+        <input type="text" class="form-control" placeholder="Buscar..." class="search-input">
+     </div>
+  </div>
+
+  <div class="d-flex">
+    <a href="#" class="nav-link"><i class="fas fa-user"></i></a>
+    <a href="#" class="nav-link"><i class="fas fa-gear"></i></a>
+  </div>
+</nav>
---
+++ resources/views/componentes/sidebar.blade.php
@@ -0,0 +1,1 @@
+<h1>Menú lateral</h1>
