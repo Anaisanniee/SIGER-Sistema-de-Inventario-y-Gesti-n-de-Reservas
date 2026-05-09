479cc13e4f62a85772f7dae48627735e59a5e935
Elias043 <molinairiarteeliasdaniel@gmail.com>
2026-05-09 17:30:01 -0500

    campos de la tabla roles

--- database/migrations/2026_05_09_183236_create_roles_table.php
+++ database/migrations/2026_05_09_183236_create_roles_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('roles', function (Blueprint $table) {
-            $table->id();
+            $table->id('rol_id');
+            $table->string('rol_nombre', 25);
             $table->timestamps();
         });
     }
