37b13a80b0d24aad73e7f82826fb595d03958827
Anais Fontalvo <anaisfontalvocarrillo@gmail.com>
2026-05-10 00:44:39 -0500

    Componente NAVBAR
    se terminó el componente navbar y se espera para configurar las paginas que van al perfil y ajuste en los iconos.

--- resources/views/componentes/navbar.blade.php
+++ resources/views/componentes/navbar.blade.php
@@ -3,16 +3,15 @@
  * La barra de navegación incluye el nombre del sistema, un campo de búsqueda y enlaces para el perfil y cerrar sesión.--}}
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div>
+ 
     <a class="navbar-brand" href="#">SIGER</a>
-  </div>
+  
 
-  <div class = "center-nav">
      <div class="buscador-container">
         <i class="fas fa-search search-icon"></i>
         <input type="text" class="form-control" placeholder="Buscar..." class="search-input">
      </div>
-  </div>
+  
 
   <div class="d-flex">
     <a href="#" class="nav-link"><i class="fas fa-user"></i></a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navbarStyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sidebarStyle.css') }}">
  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,7 +19,7 @@
                 <main class="content" style="flex: 1; padding: 20px;">
                         @yield('content')
                 </main>
-            </div>
+            
 
         <div class="sidebar" style="display: flex;">
             @include('componentes.sidebar')
