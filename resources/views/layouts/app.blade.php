cf94cc927eb6e52a46b00a6dd09842a33480c0fd
Wilmar Maya <johancastro2207@gmail.com>
2026-05-27 17:28:58 -0500

    reservas y bienvenido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,31 +1,34 @@
- <!DOCTYPE html>
- <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App</title>
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
     <link rel="stylesheet" href="{{ asset('css/componentes/ficha.tecnica.css') }}">
     <link rel="stylesheet" href="{{ asset('css/body.css') }}">
     <link rel="stylesheet" href="{{ asset('css/base/variables.css') }}">
     <link rel="stylesheet" href="{{ asset('css/componentes/navbarStyle.css') }}">
     <link rel="stylesheet" href="{{ asset('css/componentes/sidebarStyle.css') }}">
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App</title>
- </head>
- <body>
-        <header>
-            @include('componentes.navbar')
-        </header class=navbar>
+</head>
+<body>
 
-                <main class="content" style="flex: 1; padding: 20px;">
-                        @include('componentes.fichas.ficha-tecnica-activo')
-                </main>
-            
+    <header class="navbar">
+        @include('componentes.navbar')
+    </header>
+
+    <main class="content" style="flex: 1; padding: 20px;">
+        @yield('content')
+    </main>
  
-     @stack('scripts')
- </body>
- </html>  
+    @stack('scripts')
+
+</body>
+</html>
    
    
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\ReservasControllers;
+
+Route::get('/reservas', [ReservasControllers::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index-dashboard');
+});
